73601c43ccb2079662d458913ce43ade817a44b9
sitesh-bit <76125090+sitesh-bit@users.noreply.github.com>
2023-12-20 00:03:13 +0530

    Update scripted_jenkins.groovy

--- vars/scripted_jenkins.groovy
+++ vars/scripted_jenkins.groovy
@@ -1,4 +1,12 @@
 def gitdownload(repo)
 {
-  git "https://github.com/sitesh-bit/mv-app.git"
+  git "https://github.com/sitesh-bit/${repo}.git"
+}
+def mavenbuild()
+{
+  sh 'mvn package'
+}
+def deploytomcat(ip, context)
+{
+   deploy adapters: "[tomcat9(credentialsId: 'd03b691c-b1f9-467d-bd8b-c8fce3632959', path: '', url: 'http://${ip}:8080')], contextPath: '${context}', war: '**/*.war'"
 }
