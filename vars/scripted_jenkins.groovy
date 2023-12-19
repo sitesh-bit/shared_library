75a921682c60119cb49910c00690ad554a034e0e
sitesh-bit <76125090+sitesh-bit@users.noreply.github.com>
2023-12-20 01:01:08 +0530

    Update scripted_jenkins.groovy

--- vars/scripted_jenkins.groovy
+++ vars/scripted_jenkins.groovy
@@ -8,5 +8,5 @@ def mavenbuild()
 }
 def deploytomcat(ip, context)
 {
-   "deploy adapters: [tomcat9(credentialsId: 'd03b691c-b1f9-467d-bd8b-c8fce3632959', path: '', url: 'http://${ip}:8080')], contextPath: '${context}', war: '**/*.war'"
+   deploy adapters: [tomcat9(credentialsId: 'd03b691c-b1f9-467d-bd8b-c8fce3632959', path: '', url: "http://${ip}:8080")], contextPath: "${context}", war: '**/*.war'
 }
